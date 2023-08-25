04c31b8f89713170006f1398c5239a247a64ef8c
†村神† <CA01971172@st.kawahara.ac.jp>
2023-08-25 12:55:19 +0900

    temp

--- src/providers/App/DataProvider.tsx
+++ src/providers/App/DataProvider.tsx
@@ -35,6 +35,15 @@ const skillTable: boolean[][] = [
     [false, false, false, false, false, false]
 ]
 
+// boolean型の二次元配列をトグルで切り替える関数
+export function toggleSkillTable(setFunc: React.Dispatch<React.SetStateAction<boolean[][]>>, rowIndex: number, colIndex: number): void{
+    setFunc((prev) => {
+        const newArray: boolean[][] = prev.concat();
+        newArray[rowIndex][colIndex] = !prev[rowIndex][colIndex];
+        return newArray;
+    });
+}
+
 export function DataProvider({children}: {children: ReactNode}){
     const [gaps, setGaps] = useState<boolean[]>([false, false, false, false, false]);
     const [fields, setFields] = useState<boolean[]>([true, true, true, true, true, true]);
