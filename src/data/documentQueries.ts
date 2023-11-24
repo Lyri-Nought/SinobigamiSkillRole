4eaeee88f892fd4baa62166e01c5b810021025a9
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-24 12:03:31 +0900

    fix formQuery

--- src/data/documentQueries.ts
+++ src/data/documentQueries.ts
@@ -1,7 +1,7 @@
 /* ココフォリアページの要素のクエリの文字列 */
 
 // 入力フォーム
-const formQuery: string = "#root > div > div.MuiDrawer-root > div > div > div > form";
+const formQuery: string = "#root > div > div.MuiDrawer-root > div > div form";
 
 // メッセージ入力フォームの送信ボタン
 export const submitFormQuery: string = `${formQuery} button[type='submit']`;
